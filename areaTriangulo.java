97a4d510032156b6dea13e92453989a8bcd82467
Emanuel <143622434+EmanuelSanchez23@users.noreply.github.com>
2023-09-08 08:37:28 -0600

    Add files via upload

---
+++ areaTriangulo.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.practica3;
+
+
+public class areaTriangulo {
+     public static void main(String[] args) {
+        double b=10;
+        double a=15;
+        double area= b*a/2;
+         
+        
+         System.out.println(" El resultado es " + area);
+         
+         
+           
+          
+     }
+    
+}
